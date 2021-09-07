7be282eee130a8b3c8135221365158069c6dcd27
Debayo Tomisin <debayotomisin07@gmail.com>
2021-09-07 14:36:56 -0700

    set up folder structure and redux

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = (props) =>
+{
+    return(
+        <div>
+            As the day comes, as  go on, guide and watch over me oh lord.
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/SingleHouse.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const SingleHouse = (props) =>
+{
+    return(
+        <div>This is the single House component </div>
+    )
+}
+
+export default SingleHouse
