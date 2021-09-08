00706f8093751604c1568cf75808145183a0660b
Debayo Tomisin <debayotomisin07@gmail.com>
2021-09-07 22:59:31 -0700

    componenets so far

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () =>
+{
+    return(
+        <nav className="navbar">
+            <div className="nav-header">
+                <h3>AUTOCHECK-HOUSES</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
